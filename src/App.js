eb6f6fabe7cb298c839a6297817045e1e2755864
Terence Tuhinanshu <terence@tuhinanshu.com>
2020-05-29 20:26:56 -0400

    Add login form from Ant Design template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import Login from './pages/login';
 
 function App() {
     return (
         <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
+            <Router>
+                <Switch>
+                    <Route path='/'>
+                        <Login />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
 
-export default App;
+export default hot(App);
